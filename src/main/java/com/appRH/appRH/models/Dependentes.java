ac049adc81b830de114cb2ac7537d85c09b559ad
thiago-cloud <thiago.154ti@gmail.com>
2024-10-29 22:04:07 -0300

    JobWise-V1.2
    Adicionando as models Funcionario e Dependentes.

---
+++ src/main/java/com/appRH/appRH/models/Dependentes.java
@@ -0,0 +1,59 @@
+package com.appRH.appRH.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Dependentes {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	
+	@Column(unique = true)
+	private String cpf;
+	
+	private String nome;
+	private String dataNascimentoString;
+	
+	@ManyToOne// Muitos dependentes para um
+	private Funcionario funcionario;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDataNascimentoString() {
+		return dataNascimentoString;
+	}
+
+	public void setDataNascimentoString(String dataNascimentoString) {
+		this.dataNascimentoString = dataNascimentoString;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	};
+
+	
+}
---
+++ src/main/java/com/appRH/appRH/models/Funcionario.java
@@ -0,0 +1,70 @@
+package com.appRH.appRH.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Funcionario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String nome;
+	private String data;
+	private String email;
+	
+	@OneToMany(mappedBy = "funcionario", cascade = CascadeType.REMOVE)//Um funcionário que pode ter varios dependentes.
+	private List<Dependentes> dependentes;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Dependentes> getDependentes() {
+		return dependentes;
+	}
+
+	public void setDependentes(List<Dependentes> dependentes) {
+		this.dependentes = dependentes;
+	}
+	
+	
+}
